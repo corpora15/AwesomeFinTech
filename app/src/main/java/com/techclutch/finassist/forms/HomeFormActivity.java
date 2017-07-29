678cc6ad2938f9056d95f9bd7aef706132f60ce4
cobotz <cobotz87@gmail.com>
2017-07-30 07:23:17 +0800

    no message

--- app/src/main/java/com/techclutch/finassist/forms/HomeFormActivity.java
+++ app/src/main/java/com/techclutch/finassist/forms/HomeFormActivity.java
@@ -41,7 +41,7 @@ public class HomeFormActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_form);0
+        setContentView(R.layout.activity_home_form);
         ButterKnife.bind(this);
 
         etName.setText(UserDataTron.Get().GetName());
