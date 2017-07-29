e1d7365b88cfefeb39b91682a11d7cbbeb0bcae2
DESKTOP-Q864KAI\Joshua <hoongjoshua@gmail.com>
2017-07-29 20:44:42 +0800

    no message

--- FinAssistAndroid/app/src/main/java/com/techclutch/finassist/MainActivity.java
+++ FinAssistAndroid/app/src/main/java/com/techclutch/finassist/MainActivity.java
@@ -3,10 +3,15 @@ package com.techclutch.finassist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import OCR.TessOCR;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TessOCR mTessOCR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mTessOCR = new TessOCR(this, "eng");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
