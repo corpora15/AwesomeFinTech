5cc8d563edb775f68e1acebcb6801f79d5391497
arman <arman.ayadali@gemfive.com>
2017-07-29 14:00:13 +0100

    added button to open camera

---
+++ FinAssistAndroid/app/src/main/java/com/techclutch/finassist/CameraActivity.java
@@ -0,0 +1,8 @@
+package com.techclutch.finassist;
+
+/**
+ * Created by Arman on 7/29/2017.
+ */
+
+public class CameraActivity {
+}
--- FinAssistAndroid/app/src/main/java/com/techclutch/finassist/MainActivity.java
+++ FinAssistAndroid/app/src/main/java/com/techclutch/finassist/MainActivity.java
@@ -1,12 +1,23 @@
 package com.techclutch.finassist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import OCR.TessOCR;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.iv_photo)
+    ImageView ivPhoto;
+
+    private static final int CAMERA_REQUEST = 1888;
     private TessOCR mTessOCR;
 
     @Override
@@ -14,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
         mTessOCR = new TessOCR(this, "eng");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.fab)
+    void onFabClicked() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ivPhoto.setImageBitmap(photo);
+        }
     }
 }
