94014c606843465ce89fd15d1c2a9b495b830d17
arman <arman.ayadali@gemfive.com>
2017-07-30 03:08:23 +0100

    added button in document activity

--- app/src/main/java/com/techclutch/finassist/uploaddocuments/UploadDocumentActivity.java
+++ app/src/main/java/com/techclutch/finassist/uploaddocuments/UploadDocumentActivity.java
@@ -2,23 +2,23 @@ package com.techclutch.finassist.uploaddocuments;
 
 import android.content.Intent;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
 import com.techclutch.finassist.R;
 import com.techclutch.finassist.callbacks.OnDocumentTypeCallback;
 import com.techclutch.finassist.questionaires.HomeQuestionaireActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by madzirin on 7/30/2017.
@@ -28,12 +28,17 @@ public class UploadDocumentActivity extends AppCompatActivity implements OnDocum
 
     @BindView(R.id.list)
     RecyclerView recyclerView;
+    @BindView(R.id.btn_proceed)
+    Button btnProceed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_document);
         ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Please scan all documents listed below");
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new UploadDocumentsAdapter(this, getDocumentList(), this));
@@ -53,6 +58,11 @@ public class UploadDocumentActivity extends AppCompatActivity implements OnDocum
 
     @Override
     public void onListFragmentInteraction(DocumentItem item) {
+
+    }
+
+    @OnClick(R.id.btn_proceed)
+    void onProceedClicked() {
         //go to forms
         Intent intent = new Intent(this, HomeQuestionaireActivity.class);
         startActivity(intent);
